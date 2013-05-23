10d4627cea09c670b0f3657a58c062ec8571786c
Abrar Ahmed <ahmeda8@outlook.com>
2013-05-22 23:36:17 -0700

    A

--- carXapp2/Pages/MainPivotPage.xaml.cs
+++ carXapp2/Pages/MainPivotPage.xaml.cs
@@ -15,9 +15,21 @@ namespace carXapp2.Pages
 {
     public partial class MainPivotPage : PhoneApplicationPage
     {
+        private int carID;
+
         public MainPivotPage()
         {
             InitializeComponent();
+        }
+
+        private void menuAddMaint_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/AddEditMaint.xaml?carID=" + carID, UriKind.Relative));
+        }
+
+        private void menuAddFuel_click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/AddEditFuel.xaml?cid=" + carID, UriKind.Relative));
         }
     }
 }
